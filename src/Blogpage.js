77435286413dabbe42fcc33dd5cf64c779cf8901
Pratik Pansare <ppratikpansare98@gmail.com>
2024-04-18 22:11:11 +0530

    Implementing add blog functionality

--- src/Blogpage.js
+++ src/Blogpage.js
@@ -1,26 +1,55 @@
-import { useState , useEffect} from "react"
+import { useState, useEffect } from "react"
 import styles from "./Blogpage.module.css"
 
-export default function Blog(){
- 
+export default function Blog() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [Blogs, setBlogs]= useState([]);
 
-    
+  function handleSubmit(e) {
+    setBlogs([{title,content},...Blogs])
+    console.log(Blogs)
+    e.preventDefault();
+  }
 
-    return(
-        <>
-       <div className={styles.container}>
-      <h1>Blog</h1>
-      <form className={styles.form}>
-        <div className={styles.formGroup}>
-          <label htmlFor="blogTitle">Blog Title:</label>
-          <input type="text" id="blogTitle" name="blogTitle" />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="blogContent">Blog Content:</label>
-          <textarea id="blogContent" name="blogContent" rows="5"></textarea>
-        </div>
-      </form>
-    </div>
-        </>
-    )
+
+  return (
+    <>
+      <h1>Write a Blog</h1>
+      <div className={styles.container}>
+
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <label htmlFor="blogTitle">Blog Title:</label>
+            <input type="text" id="blogTitle" name="blogTitle"
+              placeholder="Enter the title here...."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)} />
+          </div>
+          <hr />
+          <div className={styles.formGroup}>
+            <label htmlFor="blogContent">Blog Content:</label>
+            <textarea id="blogContent" name="blogContent" rows="5"
+              placeholder="Blog content goes here...."
+              value={content}
+              onChange={(e) => setContent(e.target.value)}>
+
+            </textarea>
+          </div>
+          <hr />
+          <button className={styles.btn}>Submit</button>
+        </form>
+      </div>
+      <hr />
+      <h2>Blogs</h2>
+      {Blogs.map((e)=>{
+        return(
+        <><h3 className={styles.title}>{e.title}</h3>
+        <p className={styles.content}>{e.content}</p>
+        <hr/></>
+        
+        )
+      })}
+    </>
+  )
 }
